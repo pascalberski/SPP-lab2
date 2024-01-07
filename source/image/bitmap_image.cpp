6f2f0b3f6eaca8e5058bd4926d0d219e2080a425
Pascal <github@blackflagtv.de>
2024-01-07 15:17:27 +0100

    added mystery_parallel

--- source/image/bitmap_image.cpp
+++ source/image/bitmap_image.cpp
@@ -181,3 +181,61 @@ void BitmapImage::mystery() {
 		}
 	}
 }
+
+void BitmapImage::mystery_parallel(int num_of_threads) {
+	const auto construct_pixel = [](const std::uint8_t red, const std::uint8_t green, const std::uint8_t blue) -> BitmapPixel {
+		switch (BitmapImage::BitmapPixel::channel_order) {
+		case ChannelOrder::BGR:
+			return { blue, green, red };
+		case ChannelOrder::BRG:
+			return { blue, red, green };
+		case ChannelOrder::GBR:
+			return { green, blue, red };
+		case ChannelOrder::GRB:
+			return { green, red, blue };
+		case ChannelOrder::RBG:
+			return { red, blue, green };
+		case ChannelOrder::RGB:
+			return { red, green, blue };
+		}
+
+		throw std::exception{};
+		};
+
+#pragma omp parallel for num_threads(num_of_threads) default(none) shared(construct_pixel, num_of_threads)
+	for (auto y = std::uint32_t(0); y < height; y++) {
+		for (auto x = std::uint32_t(0); x < width; x++) {
+			const auto smallest_x = x;
+			const auto largest_x = (x + 2 < width) ? x + 2 : width;
+
+			const auto smallest_y = y;
+			const auto largest_y = (y + 2 < height) ? y + 2 : height;
+
+			auto red_acc = std::uint16_t{ 0 };
+			auto green_acc = std::uint16_t{ 0 };
+			auto blue_acc = std::uint16_t{ 0 };
+
+#pragma omp parallel for num_threads(num_of_threads) default(none) shared(smallest_x, smallest_y, largest_x, largest_y, red_acc, green_acc, blue_acc)
+			for (auto y_it = smallest_y; y_it < largest_y; y_it++) {
+				for (auto x_it = smallest_x; x_it < largest_x; x_it++) {
+					const auto pixel = get_pixel(y_it, x_it);
+					const auto r = pixel.get_red_channel();
+					const auto g = pixel.get_green_channel();
+					const auto b = pixel.get_blue_channel();
+
+					red_acc += r;
+					green_acc += g;
+					blue_acc += b;
+				}
+			}
+
+			const auto red_cut = static_cast<std::uint8_t>(red_acc);
+			const auto green_cut = static_cast<std::uint8_t>(green_acc);
+			const auto blue_cut = static_cast<std::uint8_t>(blue_acc);
+
+			const auto new_pixel = construct_pixel(red_cut, green_cut, blue_cut);
+
+			set_pixel(y, x, new_pixel);
+		}
+	}
+}
